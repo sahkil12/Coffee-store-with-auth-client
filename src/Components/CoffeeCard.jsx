9dda351d2c480af2a420a5c7f7849bd9508d7f20
Tazwer Shakil <tazwershakilshakil@gmail.com>
2025-10-10 11:22:00 +0600

    Added coffee data delete system

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,23 +1,43 @@
 import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
-const CoffeeCard = ({coffee}) => {
-    const {_id, photo, price, name, supplier} = coffee;
-    // delete function--
-    const handleDeleteCoffee = (id)=>{
-        alert(id)
-        // 
-        fetch(``)
-    }
+const CoffeeCard = ({ coffee }) => {
+  const { _id, photo, price, name, supplier } = coffee;
+  // delete function--
+  const handleDeleteCoffee = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // delete fetch data --
+        fetch(`http://localhost:5000/coffees/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+              if (data.deletedCount) {
+                  Swal.fire({
+            title: "Deleted!",
+            text: "Your Coffee has been deleted.",
+            icon: "success",
+          });
+            }
+          });
+      }
+    });
+  };
 
-    return (
+  return (
     <div className="flex items-center justify-around bg-[#F5F4F1] rounded-xl p-4 sm:px-6 sm:py-6  gap-6">
       {/* Product Image */}
       <div className="w-28 h-28 xl:w-46 xl:h-56">
-        <img
-          src={photo}
-          alt={name}
-          className="w-full h-full object-contain"
-        />
+        <img src={photo} alt={name} className="w-full h-full object-contain" />
       </div>
 
       {/* Product Info */}
@@ -29,27 +49,24 @@ const CoffeeCard = ({coffee}) => {
           Chef: <span className="font-normal text-neutral-500">{supplier}</span>
         </p>
         <p className="sma font-semibold">
-          Price: <span className="font-normal text-neutral-500">{price} Taka</span>
+          Price:{" "}
+          <span className="font-normal text-neutral-500">{price} Taka</span>
         </p>
       </div>
 
       {/* Action Icons */}
       <div className="flex flex-col gap-3 md:gap-5 ">
         {/* details button */}
-        <button
-          className="p-2 md:p-3 rounded-md bg-[#d9c6a1] hover:opacity-85"
-        >
+        <button className="p-2 md:p-3 rounded-md bg-[#d9c6a1] hover:opacity-85">
           <FaEye className="text-white" />
         </button>
         {/* edit details button */}
-        <button
-          className="p-2 md:p-3 rounded-md bg-black hover:opacity-85"
-        >
+        <button className="p-2 md:p-3 rounded-md bg-black hover:opacity-85">
           <FaEdit className="text-white" />
         </button>
         {/* delete button */}
         <button
-        onClick={()=> handleDeleteCoffee(_id)}
+          onClick={() => handleDeleteCoffee(_id)}
           className="p-2 md:p-3 rounded-md bg-red-500 hover:opacity-85"
         >
           <FaTrash className="text-white" />
@@ -57,7 +74,6 @@ const CoffeeCard = ({coffee}) => {
       </div>
     </div>
   );
-
 };
 
 export default CoffeeCard;

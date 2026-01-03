e887fae7b73cb9f67c69c7bebdcc914dfd21c9f6
Tazwer Shakil <tazwershakilshakil@gmail.com>
2026-01-03 12:13:40 +0600

    fixed hero section hight issue

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@
 const Hero = () => {
     return (
         <div>
-             <div style={{ backgroundImage: `url(${'/hero.png'})` , backgroundRepeat:'no-repeat', backgroundPosition:'center', backgroundSize: 'cover'}} className="h-[620px] md:h-[750px] w-full text-white flex items-center rancho justify-end">
+             <div style={{ backgroundImage: `url(${'/hero.png'})` , backgroundRepeat:'no-repeat', backgroundPosition:'center', backgroundSize: 'cover'}} className="h-[620px] md:h-[calc(100vh-110px)] w-full text-white flex items-center rancho justify-end">
           <div className="flex flex-col gap-6 md:w-2/4 mx-6">
             <h2 className="text-5xl font-medium">Would you like a Cup of Delicious Coffee?</h2>
             <p className="font-sans md:w-10/12 text-base/relaxed opacity-80">It's coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
